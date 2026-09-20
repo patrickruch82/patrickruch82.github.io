fcf694e15ed02a103ad7433598e99c37af291485
patrickruch82 <patrick.ruch@hoferco.ch>
2026-09-20 12:27:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* Familienhub – Service Worker
+   Strategie: Netzwerk zuerst, Zwischenspeicher als Rückfallebene.
+   So bekommst du immer die neuste Fassung, und ohne Empfang funktioniert
+   die App trotzdem. */
+
+const CACHE = 'familienhub-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
